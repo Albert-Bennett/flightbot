1334bbea4c11296cde9d7dd10262dd7cead41e54
Albert Bennett <albert1bennett@gmail.com>
2021-10-20 09:26:34 +0100

    Updated the startup.cs file to fix V4 update issues from the template

--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace FlightBot
 {
@@ -23,13 +24,9 @@ namespace FlightBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
-            services.AddHttpClient();
-
-            // Create the Bot Framework Adapter with error handling enabled.
+            services.AddHttpClient().AddControllers().AddNewtonsoftJson();
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
 
-            // Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
             services.AddTransient<IBot, Bot>();
             services.AddTransient<IStateManagerService, StateManagerService>();
             services.AddTransient<IAirportFindingService, AirportFindingService>();
@@ -52,13 +49,20 @@ namespace FlightBot
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseHsts();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
 
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-            app.UseWebSockets();
-
-            app.UseRouting();
+            app.UseDefaultFiles()
+                .UseStaticFiles()
+                .UseWebSockets()
+                .UseRouting()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapControllers();
+                });
         }
     }
 }
